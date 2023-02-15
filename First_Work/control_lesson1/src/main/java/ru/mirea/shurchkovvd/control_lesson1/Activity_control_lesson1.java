c6a94b32616dfb4b816ff170a697c38310cebff6
Smurflo-k <svd112002@gmail.com>
2023-02-15 22:28:35 +0300

    4 full
    5-
    6-
    7-

---
+++ First_Work/control_lesson1/src/main/java/ru/mirea/shurchkovvd/control_lesson1/Activity_control_lesson1.java
@@ -0,0 +1,17 @@
+package ru.mirea.shurchkovvd.control_lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Activity_control_lesson1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.picture);
+
+
+    }
+}
---
+++ First_Work/task_four/src/main/java/ru/mirea/shurchkovvd/task_four/Activity_four_task.java
@@ -0,0 +1,14 @@
+package ru.mirea.shurchkovvd.task_four;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_four_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.table_layout);
+    }
+}
