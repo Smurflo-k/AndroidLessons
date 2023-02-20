ca5b5cbad1e19f0a8e0a49a61eca0baf0e2aeef6
Smurflo-k <svd112002@gmail.com>
2023-02-21 01:48:14 +0300

    5 with flaws
    доделать land(для 4 и 5)

---
+++ First_Work/task_five/src/main/java/ru/mirea/shurchkovvd/task_five/Activity_five.java
@@ -0,0 +1,14 @@
+package ru.mirea.shurchkovvd.task_five;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_five extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
