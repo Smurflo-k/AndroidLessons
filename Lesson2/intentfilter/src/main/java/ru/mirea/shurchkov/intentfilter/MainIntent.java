ff79429a9ebc4e11721c613eda4922461fe13d38
Smurflo-k <svd112002@gmail.com>
2023-03-23 22:08:17 +0300

    complete IntentFilter

---
+++ Lesson2/intentfilter/src/main/java/ru/mirea/shurchkov/intentfilter/MainIntent.java
@@ -0,0 +1,32 @@
+package ru.mirea.shurchkov.intentfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainIntent extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_intent);
+    }
+    public void onMyButtonClick(View view)
+    {
+        Uri address = Uri.parse("https://www.mirea.ru/");
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+        startActivity(openLinkIntent);
+    }
+
+    public void OnAnotherBtn(android.view.View view){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Шурчков Владислав Дмитриевич");
+        startActivity(Intent.createChooser(shareIntent, "МОИ ФИО"));
+    }
+}
