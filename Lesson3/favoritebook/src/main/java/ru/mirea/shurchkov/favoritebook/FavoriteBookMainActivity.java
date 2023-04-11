6e27d7d31be31cf40a3418b7c93ee286ad1dc308
Smurflo-k <svd112002@gmail.com>
2023-04-11 14:01:00 +0300

    FavoriteBook

--- Lesson3/favoritebook/src/main/java/ru/mirea/shurchkov/favoritebook/FavoriteBookMainActivity.java
+++ Lesson3/favoritebook/src/main/java/ru/mirea/shurchkov/favoritebook/FavoriteBookMainActivity.java
@@ -1,14 +1,48 @@
 package ru.mirea.shurchkov.favoritebook;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
 
 public class FavoriteBookMainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    static final String KEY = "book_name";
+    static final String USER_MESSAGE="MESSAGE";
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_book_main);
+        textView = findViewById(R.id.textView);
+
+        ActivityResultCallback<ActivityResult> callback = new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+
+                    String userBook = data.getStringExtra(USER_MESSAGE);
+
+                    textView.setText(userBook);
+                }
+            }
+        };
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), callback);
+
+    }
+    public void getInfoAboutBook(View view) {
+        Intent intent = new Intent(this, ShareActivity.class);
+        intent.putExtra(KEY, "Путешествие к центру Земли");
+        activityResultLauncher.launch(intent);
     }
 }
---
+++ Lesson3/favoritebook/src/main/java/ru/mirea/shurchkov/favoritebook/ShareActivity.java
@@ -0,0 +1,35 @@
+package ru.mirea.shurchkov.favoritebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            TextView lol = findViewById(R.id.textView2);
+            String university = extras.getString(FavoriteBookMainActivity.KEY);
+            lol.setText(String.format("Мой любимая книга: %s", university));
+        }
+    }
+
+    public void PosilkaButton(View view) {
+        Intent data = new Intent();
+        EditText myedit = findViewById(R.id.editTextTextMultiLine);
+        String text = myedit.getText().toString();
+        data.putExtra(FavoriteBookMainActivity.USER_MESSAGE, text);
+        setResult(Activity.RESULT_OK, data);
+        finish();
+    }
+}
